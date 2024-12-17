70c890a290219dbbc2c9d1b386107e5ec499963e
adam <adambd299@gmail.com>
2024-12-17 20:29:42 +0200

    full ts change

--- src/routes/comments_routes.ts
+++ src/routes/comments_routes.ts
@@ -7,10 +7,18 @@ router.get("/", commentsController.getAllComments);
 
 router.post("/", commentsController.addComment);
 
-router.put("/:id", commentsController.updateCommentById.bind(commentsController) as express.RequestHandler);
+// router.put("/:id", commentsController.updateCommentById.bind(commentsController) as express.RequestHandler);
+
+router.put("/:id", (req,res)=>{
+    commentsController.updateCommentById(req,res);
+});
 
 router.get("/:id", commentsController.getCommentById);
 
-router.delete("/:id",commentsController.deleteCommentById.bind(commentsController) as express.RequestHandler);
+// router.delete("/:id",commentsController.deleteCommentById.bind(commentsController) as express.RequestHandler);
+
+router.delete("/:id", (req,res)=>{
+    commentsController.deleteCommentById(req,res);
+});
 
 export default router;
