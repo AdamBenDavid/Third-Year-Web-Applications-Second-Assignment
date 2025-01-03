a54e264df519aaf7cc051fba919f39a488f93797
adam <adambd299@gmail.com>
2025-01-03 17:19:28 +0200

    post swagger 0.2

--- src/routes/posts_routes.ts
+++ src/routes/posts_routes.ts
@@ -6,8 +6,8 @@ import { authMiddleware } from "../controllers/auth_controller";
 /**
  * @swagger
  * tags:
- *   name: Comments
- *   description: The Comments API
+ *   name: Posts
+ *   description: The Posts API
  */
 
 /**
@@ -41,18 +41,145 @@ import { authMiddleware } from "../controllers/auth_controller";
  *         senderId: '12345'
  */
 
+/**
+ * @swagger
+ * /posts:
+ *   get:
+ *     summary: Get all posts
+ *     tags: [Posts]
+ *     responses:
+ *       200:
+ *         description: List of all posts
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Post'
+ */
 router.get("/", postsController.getAllPosts);
 
+/**
+ * @swagger
+ * /posts/filter:
+ *   get:
+ *     summary: Get posts by sender ID
+ *     tags: [Posts]
+ *     parameters:
+ *       - in: query
+ *         name: senderId
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The ID of the user who created the post
+ *     responses:
+ *       200:
+ *         description: List of posts by sender ID
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Post'
+ *       404:
+ *         description: Posts not found
+ */
 router.get("/filter", (req, res) => {
   postsController.getPostBySenderId(req, res);
 });
 
+/**
+ * @swagger
+ * /posts:
+ *   post:
+ *     summary: Create a new post
+ *     tags: [Posts]
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Post'
+ *     responses:
+ *       201:
+ *         description: Post created successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Post'
+ */
 router.post("/", authMiddleware, postsController.addPost);
 
+/**
+ * @swagger
+ * /posts/{id}:
+ *   get:
+ *     summary: Get a post by ID
+ *     tags: [Posts]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The post ID
+ *     responses:
+ *       200:
+ *         description: Post details
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Post'
+ *       404:
+ *         description: Post not found
+ */
 router.get("/:id", postsController.getPostById);
 
+/**
+ * @swagger
+ * /posts:
+ *   delete:
+ *     summary: Delete all posts
+ *     tags: [Posts]
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: All posts deleted successfully
+ *       500:
+ *         description: Server error
+ */
 router.delete("/", authMiddleware, postsController.deletePosts);
 
+/**
+ * @swagger
+ * /posts/{id}:
+ *   put:
+ *     summary: Update a post by ID
+ *     tags: [Posts]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: Updated post ID
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Post'
+ *     responses:
+ *       200:
+ *         description: Post updated successfully
+ *       404:
+ *         description: Post not found
+ */
 router.put("/:id", authMiddleware, (req, res) => {
   postsController.updatePostById(req, res);
 });
